72acf2536395360fb266c7c3b9a337f17b983f5d
Victor Fernandes <victor.fernandes@rdcsistemas.local>
2020-03-26 15:57:41 -0300

    Backend da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+
+//Para usar json
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,42 @@
+/**
+ * Métodos HTTP
+ * 
+ * GET:     Buscar/listar informações do backend
+ * POST:    Criar informação no backend
+ * PUT:     Alterar informação no backend
+ * DELETE:  Deletar informação no backend
+ */
+
+ /**
+  * Tipos de parâmetros
+  * 
+  * Query Params:   Enviado na url (?id=16&data=20200326)
+  *     const params = req.query;
+  * 
+  * Route Params:   Utilizado para identificar recursos (/users/16)
+  *     const params = req.params;
+  * 
+  * Request Body:   Corpo da requisição (usado para criar (POST) ou alterar (PUT) uma informação)
+  */
+
+const express = require('express');
+
+const OngControler = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngControler.index);
+routes.post('/ongs', OngControler.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
